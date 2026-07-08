a02cfe4f6473702d791f51aee2ec5dc3ccb36995
JZurroAdurizDev <jabier_zurro@hotmail.com>
2026-07-08 18:43:00 +0200

    feat: add books catalog pagination

--- src/app/pages/books/books.ts
+++ src/app/pages/books/books.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+
+import { BookService } from '../../services/book/book.service';
 
 @Component({
   selector: 'app-books',
@@ -6,4 +8,61 @@ import { Component } from '@angular/core';
   templateUrl: './books.html',
   styleUrl: './books.css',
 })
-export class Books {}
+export class Books {
+  private readonly _currentPage = signal(1);
+
+  public readonly books;
+  public readonly loading;
+  public readonly error;
+
+  public readonly currentPage = this._currentPage.asReadonly();
+  public readonly itemsPerPage = 20;
+
+  public readonly totalPages = computed(() => {
+    const totalBooks = this.books().length;
+
+    if (totalBooks === 0) {
+      return 1;
+    }
+
+    return Math.ceil(totalBooks / this.itemsPerPage);
+  });
+
+  public readonly paginatedBooks = computed(() => {
+    const startIndex = (this.currentPage() - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+
+    return this.books().slice(startIndex, endIndex);
+  });
+
+  constructor(
+    private readonly _bookService: BookService
+  ) {
+    this.books = this._bookService.books;
+    this.loading = this._bookService.loading;
+    this.error = this._bookService.error;
+
+    this.loadBooks();
+  }
+
+  public loadBooks(): void {
+    this._currentPage.set(1);
+    this._bookService.loadBooks();
+  }
+
+  public previousPage(): void {
+    if (this.currentPage() === 1) {
+      return;
+    }
+
+    this._currentPage.update((currentPage) => currentPage - 1);
+  }
+
+  public nextPage(): void {
+    if (this.currentPage() === this.totalPages()) {
+      return;
+    }
+
+    this._currentPage.update((currentPage) => currentPage + 1);
+  }
+}
